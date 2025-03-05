fd967fc2d481ace2a03a3cb2a03b7b25205eb4f1
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-03-05 07:29:43 +0300

    Completed ORM

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -6,7 +6,13 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    # the engine is homebase for the database-everything on the db and python side must pass through the engine for the process to count
+    engine = create_engine('sqlite:///students.db') 
+    # create_all tells engine that any models created using base should be used to create tables
+    Base.metadata.create_all(engine)
